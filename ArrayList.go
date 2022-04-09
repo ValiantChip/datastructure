1915a06b9998917a45c05436d6cec7a4e7fad7bc
ValiantChip <lancemcstuffins@gmail.com>
2022-04-08 22:21:49 -0400

    Commit 2

--- ArrayList.go
+++ ArrayList.go
@@ -1,26 +1,27 @@
 package datastructure
 
 // ArrayList is a list of type V backed by a slice
-type arrayList[E interface{}] struct {
+type arrayList[E any] struct {
 	length   int32
 	capacity int32
 	backing  []E
 }
 
-func New[E any]() arrayList {
-	out := arrayList{
+func New[E any]() arrayList[E] {
+	capacity := int32(2)
+	out := arrayList[E]{
 		length:   0,
-		capacity: 2,
+		capacity: capacity,
 		backing:  make([]E, capacity),
 	}
 
 	return out
 }
 
-func (l arrayList) Size() int32 { return l.length }
+func (l arrayList[any]) Size() int32 { return l.length }
 
 func (l *arrayList[E]) Add(e E) {
-	if length+1 > capacity {
-		capacity *= 2
+	if l.length+1 > l.capacity {
+		l.capacity *= 2
 	}
 }
